6a9751572c23b1f5d6bbe434ac7de01a388d14cb
Kaleem09 <kuku88949@gmail.com>
2024-10-06 05:05:29 -0700

    Add files via upload

---
+++ Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header style={{ padding: '10px', background: '#f5f5f5', textAlign: 'center' }}>
+      <h1>My Web App</h1>
+    </header>
+  );
+};
+
+export default Header;
